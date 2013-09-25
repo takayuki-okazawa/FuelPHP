eaefc3eb95bb0e7551bd09a9a010f94ef035c189
Takayuki Okazawa <takayuki.hack@gmail.com>
2013-09-25 10:43:50 +0900

    Add File

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-FuelPHP
-=======
+#FuelPHP
+
+####インストール
+$ curl get.fuelphp.com/oil | sh
+
+####プロジェクト作成
+$ oil create <project_name>
+
+####依存ライブラリを動的に取得
+$ php composer.phar self-update
+$ php composer.phar update
+
+####作成したプロジェクトの実行
+Public以下が表示される。
+
+* http://localhost:8888/fuel/public/
+
+		/YUOR_PROJECT_HOME/public/index.php
+
+* http://localhost:8888/fuel/public/hello/
+
+		/YUOR_PROJECT_HOME/hoge/app/classes/controller/Welcome.php
+
+
+
+####Model
+HOME/app/classes/view/welcome/hoge.php
+
+####View
+HOME/app/views/welcome
+
+####Controler
+HOME/app/classes/controller/welcome.php
---
+++ fuel/app/logs/2013/09/24.php
@@ -0,0 +1,14 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2013-09-24 06:04:12 --> Error - date_default_timezone_get(): It is not safe to rely on the system's timezone settings. You are *required* to use the date.timezone setting or the date_default_timezone_set() function. In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier. We selected 'Asia/Tokyo' for 'JST/9.0/no DST' instead in /Applications/MAMP/htdocs/fuel/fuel/core/classes/fuel.php on line 157
+ERROR - 2013-09-24 12:12:51 --> Error - ViewModel "\View_Welcome_Hoge" could not be found. in /Applications/MAMP/htdocs/fuel/fuel/core/classes/viewmodel.php on line 70
+ERROR - 2013-09-24 12:14:24 --> Error - ViewModel "\View_Welcome_Hoge" could not be found. in /Applications/MAMP/htdocs/fuel/fuel/core/classes/viewmodel.php on line 70
+ERROR - 2013-09-24 12:15:33 --> Error - ViewModel "\View_Welcome_Hoge" could not be found. in /Applications/MAMP/htdocs/fuel/fuel/core/classes/viewmodel.php on line 70
+ERROR - 2013-09-24 12:17:39 --> Error - ViewModel "\View_Welcome_Hoge" could not be found. in /Applications/MAMP/htdocs/fuel/fuel/core/classes/viewmodel.php on line 70
+ERROR - 2013-09-24 12:18:01 --> Notice - Undefined variable: name in /Applications/MAMP/htdocs/fuel/fuel/app/views/welcome/hoge.php on line 5
+ERROR - 2013-09-24 12:20:02 --> Notice - Undefined variable: name in /Applications/MAMP/htdocs/fuel/fuel/app/views/welcome/hoge.php on line 5
+ERROR - 2013-09-24 12:20:43 --> Notice - Undefined variable: name in /Applications/MAMP/htdocs/fuel/fuel/app/views/welcome/hoge.php on line 45
+ERROR - 2013-09-24 12:21:19 --> Notice - Undefined variable: name in /Applications/MAMP/htdocs/fuel/fuel/app/views/welcome/hoge.php on line 5
+ERROR - 2013-09-24 12:21:24 --> Notice - Undefined variable: name in /Applications/MAMP/htdocs/fuel/fuel/app/views/welcome/hoge.php on line 5
+ERROR - 2013-09-24 12:22:32 --> Error - ViewModel "\View_Welcome_Hoge" could not be found. in /Applications/MAMP/htdocs/fuel/fuel/core/classes/viewmodel.php on line 70
+ERROR - 2013-09-24 12:38:32 --> Error - ViewModel "\View_Welcome_Hoge" could not be found. in /Applications/MAMP/htdocs/fuel/fuel/core/classes/viewmodel.php on line 70
